3074f524864e0a8aaa9a46ffd60942cc8a1c13a4
unknown <jvaida@asu.edu>
2024-09-05 14:54:55 -0700

    new build done for /net/embedded/ folder

---
+++ GIFT/src/edu/asu/selab/steelartt/net/embedded/message/Test.java
@@ -0,0 +1,8 @@
+package mil.arl.gift.net.embedded.message;
+
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("helloo /net/embedded/message/ ");
+    }
+}
