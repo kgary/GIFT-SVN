09fb8f6d67b91a2a41625e20199fc99379a38563
unknown <jvaida@asu.edu>
2024-09-24 16:09:32 -0700

    Removed unnecessary files

--- GIFT/src/edu/asu/selab/steelartt/net/embedded/message/codec/json/TestJSON.java
+++
@@ -1,8 +0,0 @@
-package mil.arl.gift.net.embedded.message.codec.json;
-
-public class TestJSON {
-
-    public static void main(String[] args){
-        System.out.println("helloo /net/embedded/message/codec/json/");
-    }
-}
